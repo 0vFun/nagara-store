51edce94dedc947846f921bf27c2b24ef2a78cbf
0vFun <ahmadfaadhil854@gmail.com>
2026-05-24 20:49:03 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -897,11 +897,9 @@ window.closeProductModal = function(){
 
 // ================= LOADING =================
 window.addEventListener("load", () => {
-  const loader = document.getElementById("loader");
-  if (loader) {
-    loader.style.opacity = "0";
-    setTimeout(() => loader.style.display = "none", 300);
-  }
+  renderStoreStatus();
+  updateStoreAdminUI();
+  syncStoreUI(); // 🔥 WAJIB
 });
 
 function renderStoreStatus() {
